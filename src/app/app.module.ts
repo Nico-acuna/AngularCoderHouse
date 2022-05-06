648a564b6b60a0601b044dfaddfe1506db28525d
nico-acuna <nacunapn@gmail.com>
2022-05-05 21:27:11 -0300

    Actualizacion de archivos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { StudentComponent } from './student/student.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentComponent,
+    ToolbarComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
